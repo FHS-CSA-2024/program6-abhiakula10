0912c6de2933ef1d9958087d5bf6cb567f6b7d9e
abhiakula10 <abhinavraj8258@gmail.com>
2024-08-26 15:22:56 -0700

    Update Program6.java
    finished program 6

--- Program6.java
+++ Program6.java
@@ -1,9 +1,24 @@
-//import stuff here!
+import java.util.Scanner;
 
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+public class Program6 {
+    public static void main(String[] args){
+        final double pi = 3.14;
+        double radius = 0;
+        System.out.println("Enter The New Radius: ");
+        Scanner radScanner = new Scanner(System.in); 
+        radius = radScanner.nextDouble() ;
+        double diameter = 2*radius;
+        double area = pi * radius * radius;
+        double circumference = pi * diameter;
+        System.out.println("The Diameter Of The Circle Is: " + diameter );
+        System.out.println("The Area Of The Circle Is: " + Math.round(1000 *area) / 1000.0 );
+        System.out.println("The Circumference Of The Circle Is: " + Math.round(1000*circumference)/1000.0);
+        
+        
+        
+    }
+    
+    
+    
+    
+}
